c4163e4b0e72b9a9e0abce808e67649153146566
Akuntabilitas <akuntabilitas3309@gmail.com>
2025-07-28 22:56:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,186 +1,172 @@
-// ======================
-// Hierarchical Interactive Map
-// ======================
+// script.js
 
-let map = L.map('map').setView([-7.5, 110.7], 10);
-
+const map = L.map('map').setView([-7.5, 110.6], 10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
+  attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+const layerGroups = {
+  kecamatan: L.geoJSON(null, {
+    style: { color: '#333', weight: 1, fillColor: '#8ecae6', fillOpacity: 0.6 },
+    onEachFeature: onEachKecamatan
+  }),
+  desa: L.geoJSON(null, {
+    style: { color: '#333', weight: 1, fillColor: '#219ebc', fillOpacity: 0.6 },
+    onEachFeature: onEachDesa
+  }),
+  sls: L.geoJSON(null, {
+    style: { color: '#333', weight: 1, fillColor: '#023047', fillOpacity: 0.6 },
+    onEachFeature: onEachSLS
+  })
+};
+
 let currentLevel = 'kecamatan';
-let currentKec = null;
-let currentDesa = null;
+let selectedKec = null;
+let selectedDesa = null;
+const legendList = document.getElementById('legend-list');
 
-let geojsonLayers = {
-    kecamatan: null,
-    desa: null,
-    sls: null
-};
+function resetMap() {
+  Object.values(layerGroups).forEach(layer => layer.clearLayers());
+  legendList.innerHTML = '';
+}
 
-let dataGeojson = {
-    kecamatan: null,
-    desa: null,
-    sls: null
-};
+function setLegend(items, clickHandler) {
+  legendList.innerHTML = '';
+  items.sort((a, b) => a.code.localeCompare(b.code)).forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = `${item.code} ${item.name}`;
+    li.dataset.code = item.code;
+    li.classList.add('legend-item');
+    li.addEventListener('click', () => clickHandler(item.code));
+    li.addEventListener('mouseover', () => highlightPolygon(item.code));
+    li.addEventListener('mouseout', () => resetHighlight());
+    legendList.appendChild(li);
+  });
+}
 
-let legendDiv = document.getElementById('legend');
-let backButton = document.getElementById('backButton');
-
-fetch('geojson/kecamatan.geojson')
-    .then(res => res.json())
-    .then(data => {
-        dataGeojson.kecamatan = data;
-        loadKecamatan();
-    });
-
-fetch('geojson/desa.geojson')
-    .then(res => res.json())
-    .then(data => dataGeojson.desa = data);
-
-fetch('geojson/sls.geojson')
-    .then(res => res.json())
-    .then(data => dataGeojson.sls = data);
-
-function resetLayers(level) {
-    for (let key in geojsonLayers) {
-        if (geojsonLayers[key]) {
-            map.removeLayer(geojsonLayers[key]);
-            geojsonLayers[key] = null;
-        }
+function highlightPolygon(code) {
+  map.eachLayer(layer => {
+    if (layer.feature && layer.feature.properties) {
+      const props = layer.feature.properties;
+      const layerCode = props.kdkec || props.kddesa || props.kdsls;
+      if (layerCode === code) {
+        layer.setStyle({ fillOpacity: 1, weight: 3 });
+      }
     }
-    legendDiv.innerHTML = '';
-    backButton.style.display = (level === 'kecamatan') ? 'none' : 'block';
+  });
 }
 
-function styleBase() {
-    return {
-        color: '#333',
-        weight: 1,
-        fillOpacity: 0.5,
-        fillColor: '#cccccc'
-    };
-}
-
-function highlightFeature(e) {
-    const layer = e.target;
-    layer.setStyle({
-        fillOpacity: 0.8,
-        fillColor: '#87CEFA'
+function resetHighlight() {
+  Object.entries(layerGroups).forEach(([key, layerGroup]) => {
+    layerGroup.eachLayer(layer => {
+      layer.setStyle({ fillOpacity: 0.6, weight: 1 });
     });
-
-    if (!layer._popup) {
-        const props = layer.feature.properties;
-        const name = props.nmkec || props.nmdesa || props.nmsls;
-        layer.bindTooltip(name, { sticky: true }).openTooltip();
-    }
+  });
 }
 
-function resetHighlight(e) {
-    geojsonLayers[currentLevel].resetStyle(e.target);
-    e.target.closeTooltip();
+function onEachKecamatan(feature, layer) {
+  const { kdkec, nmkec } = feature.properties;
+  layer.bindTooltip(nmkec);
+  layer.on({
+    click: () => loadDesa(kdkec),
+    mouseover: () => highlightPolygon(kdkec),
+    mouseout: () => resetHighlight()
+  });
 }
 
-function zoomToFeature(e, nextLevel) {
-    const layer = e.target;
-    const bounds = layer.getBounds();
-    map.fitBounds(bounds);
-
-    if (nextLevel === 'desa') {
-        currentLevel = 'desa';
-        currentKec = layer.feature.properties.kdkec;
-        loadDesa(currentKec);
-    } else if (nextLevel === 'sls') {
-        currentLevel = 'sls';
-        currentDesa = layer.feature.properties.kddesa;
-        loadSLS(currentKec, currentDesa);
-    }
+function onEachDesa(feature, layer) {
+  const { kddesa, nmdesa } = feature.properties;
+  layer.bindTooltip(nmdesa);
+  layer.on({
+    click: () => loadSLS(kddesa),
+    mouseover: () => highlightPolygon(kddesa),
+    mouseout: () => resetHighlight()
+  });
 }
 
-function addLegend(features, labelProp, kodeProp, onClick) {
-    const sorted = features.sort((a, b) => a.properties[kodeProp].localeCompare(b.properties[kodeProp]));
-    sorted.forEach(f => {
-        const kode = f.properties[kodeProp];
-        const nama = f.properties[labelProp];
-        const div = document.createElement('div');
-        div.className = 'legend-item';
-        div.textContent = `${kode} ${nama}`;
-        div.addEventListener('click', () => onClick(f));
-        div.addEventListener('mouseenter', () => highlightFeature({ target: geojsonLayers[currentLevel]._layers[f.id] }));
-        div.addEventListener('mouseleave', () => resetHighlight({ target: geojsonLayers[currentLevel]._layers[f.id] }));
-        legendDiv.appendChild(div);
-    });
+function onEachSLS(feature, layer) {
+  const { kdsls, nmsls } = feature.properties;
+  layer.bindTooltip(nmsls);
+  layer.on({
+    click: () => zoomToLayer(layer),
+    mouseover: () => highlightPolygon(kdsls),
+    mouseout: () => resetHighlight()
+  });
+}
+
+function zoomToLayer(layer) {
+  map.fitBounds(layer.getBounds());
 }
 
 function loadKecamatan() {
-    resetLayers('kecamatan');
-    geojsonLayers.kecamatan = L.geoJSON(dataGeojson.kecamatan, {
-        style: styleBase,
-        onEachFeature: (feature, layer) => {
-            layer.on({
-                mouseover: highlightFeature,
-                mouseout: resetHighlight,
-                click: (e) => zoomToFeature(e, 'desa')
-            });
-        }
-    }).addTo(map);
-
-    addLegend(dataGeojson.kecamatan.features, 'nmkec', 'kdkec', feature => {
-        const layer = geojsonLayers.kecamatan.getLayers().find(l => l.feature === feature);
-        if (layer) layer.fire('click');
+  resetMap();
+  currentLevel = 'kecamatan';
+  selectedKec = null;
+  selectedDesa = null;
+  fetch('data/final_kec_202413309.geojson')
+    .then(res => res.json())
+    .then(data => {
+      layerGroups.kecamatan.addData(data).addTo(map);
+      map.fitBounds(layerGroups.kecamatan.getBounds());
+      const items = data.features.map(f => ({
+        code: f.properties.kdkec,
+        name: f.properties.nmkec
+      }));
+      setLegend(items, loadDesa);
     });
 }
 
 function loadDesa(kdkec) {
-    resetLayers('desa');
-    const filtered = dataGeojson.desa.features.filter(f => f.properties.kdkec === kdkec);
-    geojsonLayers.desa = L.geoJSON(filtered, {
-        style: styleBase,
-        onEachFeature: (feature, layer) => {
-            layer.on({
-                mouseover: highlightFeature,
-                mouseout: resetHighlight,
-                click: (e) => zoomToFeature(e, 'sls')
-            });
-        }
-    }).addTo(map);
-
-    addLegend(filtered, 'nmdesa', 'kddesa', feature => {
-        const layer = geojsonLayers.desa.getLayers().find(l => l.feature === feature);
-        if (layer) layer.fire('click');
+  resetMap();
+  currentLevel = 'desa';
+  selectedKec = kdkec;
+  fetch('data/final_desa_202413309.geojson')
+    .then(res => res.json())
+    .then(data => {
+      const filtered = {
+        type: 'FeatureCollection',
+        features: data.features.filter(f => f.properties.kdkec === kdkec)
+      };
+      layerGroups.desa.addData(filtered).addTo(map);
+      map.fitBounds(layerGroups.desa.getBounds());
+      const items = filtered.features.map(f => ({
+        code: f.properties.kddesa,
+        name: f.properties.nmdesa
+      }));
+      setLegend(items, loadSLS);
     });
 }
 
-function loadSLS(kdkec, kddesa) {
-    resetLayers('sls');
-    const filtered = dataGeojson.sls.features.filter(f => f.properties.kdkec === kdkec && f.properties.kddesa === kddesa);
-    geojsonLayers.sls = L.geoJSON(filtered, {
-        style: styleBase,
-        onEachFeature: (feature, layer) => {
-            layer.on({
-                mouseover: highlightFeature,
-                mouseout: resetHighlight
-            });
-        }
-    }).addTo(map);
-
-    const bounds = geojsonLayers.sls.getBounds();
-    map.fitBounds(bounds);
-
-    addLegend(filtered, 'nmsls', 'kdsls', feature => {
-        const layer = geojsonLayers.sls.getLayers().find(l => l.feature === feature);
-        if (layer) map.fitBounds(layer.getBounds());
+function loadSLS(kddesa) {
+  resetMap();
+  currentLevel = 'sls';
+  selectedDesa = kddesa;
+  fetch('data/final_sls_202413309.geojson')
+    .then(res => res.json())
+    .then(data => {
+      const filtered = {
+        type: 'FeatureCollection',
+        features: data.features.filter(f => f.properties.kddesa === kddesa)
+      };
+      layerGroups.sls.addData(filtered).addTo(map);
+      map.fitBounds(layerGroups.sls.getBounds());
+      const items = filtered.features.map(f => ({
+        code: f.properties.kdsls,
+        name: f.properties.nmsls
+      }));
+      setLegend(items, code => {
+        const layer = layerGroups.sls.getLayers().find(l => l.feature.properties.kdsls === code);
+        if (layer) zoomToLayer(layer);
+      });
     });
 }
 
-backButton.addEventListener('click', () => {
-    if (currentLevel === 'desa') {
-        currentLevel = 'kecamatan';
-        currentKec = null;
-        loadKecamatan();
-    } else if (currentLevel === 'sls') {
-        currentLevel = 'desa';
-        currentDesa = null;
-        loadDesa(currentKec);
-    }
+document.getElementById('back-btn').addEventListener('click', () => {
+  if (currentLevel === 'sls') {
+    loadDesa(selectedKec);
+  } else if (currentLevel === 'desa') {
+    loadKecamatan();
+  }
 });
+
+loadKecamatan();
